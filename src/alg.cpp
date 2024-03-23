12b00862635925a6145959238df76b133513d115
olesiatuliapkina <151723481+olesiatuliapkina@users.noreply.github.com>
2024-03-23 23:33:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,24 +1,6 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-    int left = 0;
-    int right = size - 1;
-    int counter = 1;
-    while (left <= right)
-        int middle = (left + right)/2;
-        if (*(arr + middle) == value) 
-                int i = middle - 1;
-                while (*(arr + (i)) == value) 
-                    counter += 1;
-                    i--;
-                int j = middle + 1;
-                while (*(arr + (j)) == value) 
-                    counter += 1;
-                    j++;     
-            return counter;
-        else if (*(arr + middle) < value)
-            left = middle + 1;
-        else
-            right = middle - 1;
-    return 0; 
+  //  поместить сюда реализацию алгоритма
+  return 0; // если ничего не найдено
 }
